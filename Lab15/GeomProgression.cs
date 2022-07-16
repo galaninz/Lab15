2ca1f1cdcf4dd79c5f96ab36bfa916680065e4df
Zakhar Galanin <zakhargalanin@gmail.com>
2022-07-15 23:05:31 -0700

    Add project files.

---
+++ Lab15/GeomProgression.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    public class GeomProgression
+    {
+        int startValue;
+        int step;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void setStart(int startNumber)
+        {
+            startValue = startNumber;
+            currentValue = startValue;
+        }
+
+        public void setStep(int step)
+        {
+            this.step = step;
+        }
+    }
+}
---
+++ Lab15/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Да я грипповал, очень много кто болеет у нас. Берегите себя.
+            ArithProgression arithProgression = new ArithProgression();
+            GeomProgression geomProgression = new GeomProgression();
+            Console.WriteLine("Введите количество шагов");
+            int stepNumbers = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Какой тип прогрессии будем смотреть?\n 1 - Арифметическая\n 2 - Геометрическая");
+            int type = Convert.ToInt32(Console.ReadLine());
+            if (type == 1)
+            {
+                Console.WriteLine("Введите стартовое значение");
+                arithProgression.setStart(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Введите шаг последовательности");
+                arithProgression.setStep(Convert.ToInt32(Console.ReadLine()));
+                for (int i = 0; i < stepNumbers; i++)
+                {
+                    Console.WriteLine(arithProgression.getNext());
+                }
+            }
+            else if (type == 2)
+            {
+                Console.WriteLine("Введите стартовое значение");
+                geomProgression.setStart(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Введите шаг последовательности");
+                geomProgression.setStep(Convert.ToInt32(Console.ReadLine()));
+                for (int i = 0; i < stepNumbers; i++)
+                {
+                    Console.WriteLine(geomProgression.getNext());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверный код операции");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
